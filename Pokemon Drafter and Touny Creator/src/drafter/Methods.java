70bb34fc2f292edcf923f10b2dfd157cb00b4e9e
Madlamb <madlmab420@gmail.com>
2014-01-12 10:25:11 -0800

    hehue

--- Pokemon Drafter and Touny Creator/src/drafter/Methods.java
+++ Pokemon Drafter and Touny Creator/src/drafter/Methods.java
@@ -3,6 +3,11 @@ import java.io.*;
 
 public class Methods {
 	//Writes to a file in the format Chooser then Pokemon chosen
+	public void createDir(){
+		String mainDirName = "C:" + File.separator + "Pokemon Showdown Drafter";
+		File mainDir = new File(mainDirName);
+		mainDir.mkdir();
+	}
 	public void writeToFile(){
 		
 	}

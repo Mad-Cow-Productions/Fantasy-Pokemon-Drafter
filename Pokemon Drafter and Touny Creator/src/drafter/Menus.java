f6302fec45d2ad13cd39bec5c1d80ef406ba14bc
WJCOOK <willnot3@gmail.com>
2014-01-12 14:09:24 -0800

    Here I resolved the problem but this version still needs polishing

--- Pokemon Drafter and Touny Creator/src/drafter/Menus.java
+++ Pokemon Drafter and Touny Creator/src/drafter/Menus.java
@@ -11,7 +11,7 @@ import javax.swing.*;
 
 
 public class Menus {
-	private static JFrame frame;
+	/*private static JFrame frame;
 	private JLabel label;
 	private JButton addText;
 	private static JTextField textField;
@@ -22,9 +22,86 @@ public class Menus {
 	
 	private static int Width;
 	private static int Height; //Here is where I define the varibles for the pos for the text boxes
+	*/
+	
+	private static JFrame enterNames;
+	private static JTextField textFieldNames;
+	private static JButton confirmNumber;
+	public  static String[] contestantsNames;
+	private static int numberOfContestants;
+	private static JButton moveOn;
+	private static int count = 0;
+	
 	
 	public Menus(){
-		frame = new JFrame("Pokemon Drafter");
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		enterNames = new JFrame();
+		enterNames.setLocation(0,0);							//this is where the JFrame is created and configured
+		enterNames.setLayout(null);
+		enterNames.setSize(tk.getScreenSize().width/2, tk.getScreenSize().height/2); //gets the screen size and sets the JFrame to fit the whole screen
+		enterNames.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		textFieldNames = new JTextField();
+		textFieldNames.setBounds(100,50, 200, 25);
+		enterNames.add(textFieldNames);
+		
+		
+		confirmNumber = new JButton();
+		confirmNumber.setBounds(325, 50, 50, 25);
+		confirmNumber.setText("#");
+		confirmNumber.addActionListener(new confirmNumber());
+		
+		enterNames.add(confirmNumber);
+		
+		enterNames.setVisible(true);
+	}
+	
+	static class confirmNumber implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			numberOfContestants = Integer.parseInt(textFieldNames.getText());
+			contestantsNames = new String[numberOfContestants];
+			enterNames.setVisible(false);
+			enterNames.remove(confirmNumber);
+			textFieldNames.setText(" ");
+			moveOn = new JButton();
+			moveOn.setBounds(325, 50, 50, 25);
+			moveOn.addActionListener(new moveOn());
+			enterNames.add(moveOn);
+			enterNames.setVisible(true);
+			
+			
+			
+			
+			
+		}
+		
+	}
+	
+	static class moveOn implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String text = textFieldNames.getText();
+			
+			contestantsNames[count] = text;
+			enterNames.setVisible(false);
+			textFieldNames.setText(" ");
+			count++;
+			if(count >= numberOfContestants){
+				textFieldNames.setText("No More Writing");
+				textFieldNames.setEditable(false);
+				enterNames.setVisible(true);
+			}else{
+			enterNames.setVisible(true);
+			}
+			
+		}
+		
+	}
+		/*frame = new JFrame("Pokemon Drafter");
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		frame.setLocation(0,0);							//this is where the JFrame is created and configured
 		frame.setLayout(null);
@@ -101,21 +178,21 @@ public class Menus {
 			JLabel testLabel = new JLabel();
 			testLabel.setText(text);
 			testLabel.setBounds(40,40,100,50);
-			/*if(text.contains(",")) {
+			if(text.contains(",")) {
 				testLabel.setText("False");
 			}else{
 				testLabel.setText(text);
-			}*/
+			}
 			testFrame.add(testLabel);
 			testFrame.setVisible(true);
 		
 		}
 		
 	}
-	
+	*/
 	public static void main(String[] args) {
 		Menus test = new Menus();
-		test.setOpen(true);
+		
 	
 	}
 

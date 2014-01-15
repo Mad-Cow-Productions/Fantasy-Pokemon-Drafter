545beab9c7b67a7a35c0f67c92773dfd76a6183e
Madlamb <madlmab420@gmail.com>
2014-01-14 18:59:40 -0800

    Added get nxt round order, rnd, and valid choice checker

--- Pokemon Drafter and Touny Creator/src/drafter/Methods.java
+++ Pokemon Drafter and Touny Creator/src/drafter/Methods.java
@@ -7,6 +7,9 @@ public class Methods {
 	File draftList = new File(fileName);
 	String mainDirName = "C:" + File.separator + "Pokemon Showdown Drafter";
 	File mainDir = new File(mainDirName);
+	String configFileName = "C:" + File.separator + "Pokemon Showdown Drafter" + File.separator + "Valid Pokemon Config.txt";
+	File configFile = new File(configFileName);
+	private String validPokemon;
 
 	public void createDir(){
 		mainDir.mkdir();
@@ -42,7 +45,57 @@ public class Methods {
 	public int pickPerson(String[] drafterList){
 		Random rnd = new Random();
 		int rand = rnd.nextInt(numberOfContestants);
+		return rand;
 	}
+	public boolean getNextRoundOrder(int roundNumber){ //this method will choose the order of the next round for each contestant
+
+        if(roundNumber > 10) {
+                return false;
+        }else{
+
+                for(int i=0; i > numberOfContestants; i++){
+
+                        int newPos = -(namesOfContestants[i][1]) +(numberOfContestants - 1);
+                        namesofContestants[i][1] = newPos;
+
+                }
+                
+                return true;
+                
+
+        }
+
+	}
+	public boolean checkValidChoice(String pokemonChoice){   //this method will check if the pokemonChoice is a valid one
+
+        if(validPokemon.contains(pokemonChoice)){
+
+        return true;
+        
+        }else{
+
+        return false;   
+        
+        }
+       
+
+}
+	public void readConfig() {
+        byte[] configByte = new byte[0];
+        try {
+        FileInputStream configStream = new FileInputStream(configFile);
+        int configLength = configStream.available();
+        configByte = new byte[configLength];
+        configStream.read(configByte);
+        configStream.close();
+        String validPokemon = new String(configByte);
+        } catch (FileNotFoundException fnfe) {
+        	
+        } catch (IOException ioe){
+        	
+        }
+	}
+	
 	public Methods(){
 		
 	}
